309b9075c1d419cd1f443296392368381de74fee
Moises Gonzalez <moises.gonzalez@flixbus.com>
2020-03-26 12:20:07 +0100

    Adds all functionality for test

--- static/update-time-worker.js
+++ static/update-time-worker.js
@@ -2,7 +2,7 @@ onmessage = ({ data }) => {
   const end = new Date(data * 1000);
   const start = new Date();
 
-  const timeDiff = Math.floor((end.getTime() - start.getTime()) / 1000);
+  const timeDiff = Math.floor((end.getTime() - start.getTime()));
 
   setTimeout(() => {
     postMessage('Update weather');
